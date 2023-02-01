35938f15fea4e0e8adaa353ff69e0f5906e5d9b9
Erdoo <erdoo.kuhwa@gmail.com>
2023-02-01 17:30:45 -0600

    getting started with Cypress

---
+++ cypress/e2e/resume.cy.js
@@ -0,0 +1,10 @@
+describe('my resume challenge app', () => {
+    beforeEach(() => {
+        // I'm telling Cypress to go to my app
+        cy.visit('https://resume.everlycloud.tk')
+    })
+
+    it('displays my resume index.html page', () => {
+        // 
+    })
+})
